d84235cda45d65323eed339ac4a82799aa7e2fd5
Sameer Suman <sameersuman2403@gmail.com>
2022-01-11 20:20:48 +0530

    d3

--- DP/Print first n Fibonacci Numbers.cpp
+++ DP/Print first n Fibonacci Numbers.cpp
@@ -2,7 +2,8 @@ vector<long long> printFibb(int n)
     {
         //code here
           vector<long long>dp(n);
-       dp[0]=dp[1]=1;
+       dp[0]=0;
+       dp[1]=1;
        for(int i=2; i<n; i++)
        dp[i]=dp[i-1]+dp[i-2];
        return dp;

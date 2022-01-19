72d682cecc9f216c2fef235f3a5e4cb90bdff167
Sameer Suman <sameersuman2403@gmail.com>
2022-01-19 17:28:47 +0530

    gfg

---
+++ Array/Three way partitioning.cpp
@@ -0,0 +1,28 @@
+Input: 
+n = 5
+A[] = {1, 2, 3, 3, 4}
+[a, b] = [1, 2]
+Output: 1
+Explanation: One possible arrangement is:
+{1, 2, 3, 3, 4}. If you return a valid
+arrangement, output will be 1.
+
+
+
+ void threeWayPartition(vector<int>& array,int a, int b)
+    {
+        // code here 
+         int n= array.size();
+         int start = 0, end = n-1;
+     
+        for (int i=0; i<=end;)
+        {
+            if (array[i] < a)
+                swap(array[i++], array[start++]);
+            else if (array[i] > b)
+                swap(array[i], array[end--]);
+            else
+                i++;
+        }
+       
+    }

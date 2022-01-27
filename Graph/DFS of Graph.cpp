0e5c06787bf5fd752c0524a93d9a6829340bf330
Sameer Suman <sameersuman2403@gmail.com>
2022-01-27 19:23:16 +0530

    gfg

---
+++ Graph/DFS of Graph.cpp
@@ -0,0 +1,26 @@
+void dfs(int node, vector<int> &vis, vector<int> adj[], vector<int> &storeDfs)
+{
+
+    storeDfs.push_back(node);
+    vis[node] = 1;
+    for (auto it : adj[node])
+    {
+        if (!vis[it])
+            dfs(it, vis, adj, storeDfs);
+    }
+}
+vector<int> dfsOfGraph(int V, vector<int> adj[])
+{
+    // Code here
+    vector<int> vis(V, 0);
+    vector<int> storeDfs;
+
+    for (int i = 0; i < V; i++)
+    {
+        if (!vis[i])
+        {
+            dfs(i, vis, adj, storeDfs);
+        }
+    }
+    return storeDfs;
+}
